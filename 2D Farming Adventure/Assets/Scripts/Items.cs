cf62f96604181d52e3080b78d13bfc9feef7bf83
Maren <59347125+Zyrissa@users.noreply.github.com>
2020-05-16 22:41:01 +0200

    Database
    Adding an Item and Carakter Database System

---
+++ 2D Farming Adventure/Assets/Scripts/Items.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Items 
+{
+    public int itemID;
+    public string title;
+    public string description;
+    //public Sprite icon;
+
+    public GameObject itemObject;
+
+    public float range;
+    public float damage;
+}
