4455e702137fd77991a74d8f0d2b348e10f54c4e
Wiebke Schöbel <wieb-schoebel@gmx.de>
2020-06-03 18:50:55 +0200

    [2F1-T15] added comments in movement scripts
    - added comments to the scripts for movement
    - added movement stop while Inventory is opened

--- 2D Farming Adventure/Assets/Services/PlayerMovementServiceImpl.cs
+++ 2D Farming Adventure/Assets/Services/PlayerMovementServiceImpl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace Assets.Services
 {
+    /// <summary>
+    /// Player Movement Service Implementation
+    /// </summary>
     class PlayerMovementServiceImpl : MovementServiceImpl
     {
         Animator animator;
@@ -12,11 +15,23 @@ namespace Assets.Services
             this.animator = animator;
         }
 
+        /// <summary>
+        /// public method to create a new service implementation instance
+        /// </summary>
+        /// <param name="animator"></param>
+        /// <returns>new instance of this service</returns>
         public static PlayerMovementServiceImpl Create(Animator animator)
         {
             return new PlayerMovementServiceImpl(animator);
         }
 
+        /// <summary>
+        /// calculates the new position of the Player
+        /// </summary>
+        /// <param name="currentPosition"></param>
+        /// <param name="movementSpeed"></param>
+        /// <param name="enteredCollision"></param>
+        /// <returns>new actual position</returns>
         public override Vector2 CalculateNewPosition(Vector2 currentPosition, float movementSpeed, bool enteredCollision)
         {
             float horizontalValue = Input.GetAxis("Horizontal");
